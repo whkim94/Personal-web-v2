68d986466685777d22a0aeaa8acf37decd965bfc
Steve, Kim <18128800+whkim94@users.noreply.github.com>
2024-01-24 16:40:15 -0800

    Fix: layout and add sub categories of skills

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-main>
-    <v-container fluid>asdf
-      <v-app-bar elevation="0" class="px-16">
+  <v-main style="background: grey">
+    <v-container class="py-16">
+      <!-- <v-app-bar elevation="0" class="px-16">
         <v-app-bar-title>
           <Logo />
         </v-app-bar-title>
@@ -15,128 +15,170 @@
             {{ item.text }}
           </v-btn>
         </v-toolbar-items>
-      </v-app-bar>
+      </v-app-bar> -->
 
-      <!-- Landing/Introduction section -->
-      <section id="introduction">
-        <v-sheet class="pa-16">
-          <v-row align="center" justify="center" no-gutters>
-            <v-col cols="12" md="5" class="py-12">
-              <div>
-                <div class="text-h4">
-                  Hai! I AM
-                </div>
-                <div class="text-h3 text-green">
-                  JONATHAN KIM
-                </div>
+      <v-row justify="center" no-gutters>
+        <v-col md="4">
+          <!-- Name section -->
+          <v-sheet class="pa-8">
+
+            <div>
+              <div class="text-h3 text-green">
+                JONATHAN KIM
               </div>
 
-              <div class="py-4">
-                <p class="text-subtitle-2">
-                  - I am a Full-Stack Developer - with a passion for creating
-                  user-friendly and efficient web applications. With a strong
-                  background in both frontend and backend technologies, I have
-                  developed a wide range of projects, from e-commerce sites to
-                  custom web applications.
-                </p>
+              <div class="text-h5">
+                Full Stack developer
               </div>
 
-              <div class="py-4">
-                <v-btn
-                  variant="outlined"
-                  color="green"
-                  href="#portfolio"
+              <div class="py-6">
+                I build from Front to Back.
+              </div>
+            </div>
+
+            <div class="py-4">
+              <v-btn
+                variant="outlined"
+                color="green"
+                href="#portfolio"
+              >
+                Let's Talk!
+              </v-btn>
+            </div>
+          </v-sheet>
+        </v-col>
+
+        <v-col md="4">
+          <!-- About Me section -->
+          <section id="about">
+            <div class="pa-8">
+              <p class="text-h6">
+                I am a Full-Stack Developer - with a passion for creating
+                user-friendly and efficient web applications. With a strong
+                background in both frontend and backend technologies, I have
+                developed a wide range of projects, from e-commerce sites to
+                custom web applications.
+              </p>
+            </div>
+
+            <div>
+              <v-timeline side="end">
+                <v-timeline-item
+                  v-for="exp in experiences"
+                  :key="exp.duration"
+                  dot-color="green"
+                  size="small"
+                  hide-opposite
                 >
-                  Let's Talk!
-                </v-btn>
-              </div>
-            </v-col>
+                  <v-card class="elevation-0">
+                    <v-card-title class="text-h5">
+                      {{ exp.name }}
+                    </v-card-title>
+                    <v-card-subtitle>
+                      {{ exp.company }} . {{ exp.level }}
+                    </v-card-subtitle>
+                    <v-card-subtitle>
+                      {{ exp.duration }}
+                    </v-card-subtitle>
+                    <v-card-subtitle>
+                      {{ exp.location }}
+                    </v-card-subtitle>
 
-            <v-col cols="12" md="7">
-              <v-sheet class="d-flex justify-center">
-                <div>
-                  <div class="shape-behind"/>
-                  <v-img class="profile-picture" src="/My-image.png" alt="Your Name"/>
-                </div>
-              </v-sheet>
-            </v-col>
-          </v-row>
-        </v-sheet>
-      </section>
+                    <v-card-text style="white-space: pre;">
+                      {{ exp.desc }}
+                    </v-card-text>
+                  </v-card>
+                </v-timeline-item>
+              </v-timeline>
+            </div>
 
-      <!-- About Me section -->
-      <section id="about">
-        <v-row>
-          <v-col cols="12" md="4">
-            <h2>About Me</h2>
-          </v-col>
-          <v-col cols="12" md="8">
-            <p/>
-          </v-col>
-        </v-row>
-      </section>
+            <v-row>
+              <v-col cols="12" md="4">
+                <h2>About Me</h2>
+              </v-col>
+              <v-col cols="12" md="8">
+                <p/>
+              </v-col>
+            </v-row>
+          </section>
 
-      <!-- Skills section -->
-      <section id="skills">
-        <v-row>
-          <v-col cols="12">
-            <h2>Skills</h2>
-          </v-col>
-          <v-col
-            v-for="skill in skills"
-            :key="skill.name"
-            cols="6" sm="4" md="3"
-          >
-            <v-card>
-              <v-card-title>
-                {{ skill.name }}
-              </v-card-title>
-              <v-card-subtitle>
-                {{ skill.level }}
-              </v-card-subtitle>
-              <v-card-text>
-                <v-progress-linear
-                  v-model="skill.value"
-                  color="green"
-                />
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </section>
+          <!-- Skills section -->
+          <section id="skills">
+            <div>
+              Skills
+            </div>
+            <v-row>
+              <v-col cols="12">
+                <v-card>
+                  <v-card-title>Frontend</v-card-title>
 
-      <!-- Portfolio section -->
-      <section id="portfolio">
-        <v-row>
-          <v-col cols="12">
-            <h2>Portfolio</h2>
-          </v-col>
-          <v-col
-            v-for="project in projects"
-            :key="project.title"
-            cols="12" sm="6" md="4"
-          >
-            <v-card>
-              <v-img :src="project.image"/>
-              <v-card-title>
-                {{ project.title }}
-              </v-card-title>
-              <v-card-subtitle>
-                {{ project.description }}
-              </v-card-subtitle>
-              <v-card-actions>
-                <v-btn
-                  text
-                  color="primary"
-                  :href="project.link"
-                >
-                  View Project
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </section>
+                  <v-card-item>
+                    <v-avatar v-for="icon in frontends" :key="icon.name">
+                      <v-img :src="icon.icon" :alt="icon.name"/>
+                    </v-avatar>
+                  </v-card-item>
+                </v-card>
+              </v-col>
+
+              <v-col
+                v-for="skill in skills"
+                :key="skill.name"
+                cols="6" sm="4" md="3"
+              >
+                <v-card>
+                  <v-card-title>
+                    {{ skill.name }} <v-img :src="skill.icon"/>
+                  </v-card-title>
+
+                  <v-card-subtitle>
+                    {{ skill.level }}
+                  </v-card-subtitle>
+                  <v-card-text>
+                    <v-progress-linear
+                      v-model="skill.value"
+                      color="green"
+                    />
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+          </section>
+
+          <!-- Portfolio section -->
+          <section id="portfolio">
+            <v-row>
+              <v-col cols="12">
+                <h2>Portfolio</h2>
+              </v-col>
+              <v-col
+                v-for="project in projects"
+                :key="project.title"
+                cols="12" sm="6" md="4"
+              >
+                <v-card>
+                  <v-img :src="project.image"/>
+                  <v-card-title>
+                    {{ project.title }}
+                  </v-card-title>
+                  <v-card-subtitle>
+                    {{ project.description }}
+                  </v-card-subtitle>
+                  <v-card-actions>
+                    <v-btn
+                      text
+                      color="primary"
+                      :href="project.link"
+                    >
+                      View Project
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+            </v-row>
+          </section>
+        </v-col>
+      </v-row>
+
     </v-container>
   </v-main>
 </template>
@@ -149,15 +191,96 @@ const menuItems = ref([
   // Add more items as needed
 ]);
 
+const experiences = ref([
+  {
+    name: 'Full-Stack Developer | DevOps',
+    duration: 'Jul 2023 - Present',
+    company: '',
+    level: 'Freelance',
+    location: 'remote',
+    desc: ''
+  },
+  {
+    name: 'Full-Stack Developer | DevOps',
+    duration: 'Jan 2023 - Jul 2023',
+    company: 'Interfit Worldwide Inc',
+    level: 'Freelance',
+    location: 'Irvine / CA',
+    desc: 'Oversaw development milestones from inception to final delivery.\nBoosted daily traffic by 50%.\nImplemented enhancements to both backend and frontend, improving performance and introducing new features.\nEstablished release and development environments using Docker.\nDeployed microservices using AWS Fargate.\nCreated admin page leveraging Google Analytics API for monitoring daily website status.\nConfigured GitHub Actions for automated CI/CD.'
+  },
+  {
+    name: 'Full-Stack Developer',
+    duration: 'May 2021 - Jan 2023',
+    company: 'BASF',
+    level: 'Freelance',
+    location: 'San Diego / CA',
+    desc: 'Oversaw full-cycle process, from designing layouts using Figma to building projects with Nuxt.js.\nEnhanced FastAPI backend and streamlined deployment using Docker and GitLab CI/CD.\nCreated sophisticated tool for migrating from legacy software.\nCollaborated with scientists to develop research tools, increasing their work efficiency.\nEnhanced user experience by providing tailored suggestions.\n'
+  },
+  {
+    name: 'Full-Stack Developer',
+    duration: 'Jan 2023 - Jul 2023',
+    company: 'Interfit',
+    level: 'Freelance',
+    location: 'Irvine / CA',
+    desc: 'Developed web application from scratch as early team member at startup.\nImplemented Twilio\'s Video and Voice bi-directional conferencing features.\nDeployed Docker-based microservices, including Nginx, frontend, server, workers, and more, on AWS ECS.\nConfigured scheduled tasks using Django Celery and AWS Cluster Scheduler.\nIntroduced user chat functionality, leveraging Django with Daphne\'s ASGI and Socket.io.\nUtilized Nuxt.js for SEO optimization and Vuetify for enhanced UI/UX design.'
+  },
+]);
+
+const languages = ref([
+  {
+    name: 'Python', level: 'Expert', value: 90, icon: '/icons/python.svg'
+  },
+  {
+    name: 'Javascript', level: 'Intermediate', value: 80, icon: '/icons/javascript.svg'
+  },
+  {
+    name: 'Typescript', level: 'Intermediate', value: 80, icon: '/icons/typescript.svg'
+  },
+]);
+
+const frontends = ref([
+  {
+    name: 'Vue.js', level: 'Advanced', value: 75, icon: '/icons/vue.svg'
+  },
+  {
+    name: 'Nuxt.js', level: 'Intermediate', value: 70, icon: '/icons/nuxt-icon.svg'
+  },
+  {
+    name: 'React.js', level: 'Intermediate', value: 75, icon: '/icons/react.svg'
+  },
+]);
+
+const backends = ref([
+  {
+    name: 'Django', level: 'Advanced', value: 75, icon: '/icons/django.svg'
+  },
+  {
+    name: 'FastAPI', level: 'Intermediate', value: 70, icon: '/icons/fastapi-icon.svg'
+  },
+  {
+    name: 'Node.js', level: 'Intermediate', value: 75, icon: '/icons/nodejs-icon.svg'
+  },
+]);
+
+const styles = ref([
+  {
+    name: 'Vuetify', level: 'Advanced', value: 75, icon: '/icons/vuetifyjs.svg'
+  },
+]);
+
+const DevOps = ref([
+  {
+    name: 'AWS', level: 'Intermediate', value: 75, icon: '/icons/aws.svg'
+  },
+]);
+
 const skills = ref([
-  { name: 'HTML/CSS', level: 'Expert', value: 90 },
-  { name: 'JavaScript', level: 'Advanced', value: 80 },
-  { name: 'Vue.js', level: 'Advanced', value: 75 },
-  { name: 'Nuxt.js', level: 'Intermediate', value: 70 },
-  { name: 'Vuetify', level: 'Advanced', value: 75 },
-  { name: 'Node.js', level: 'Intermediate', value: 65 },
-  { name: 'Python', level: 'Intermediate', value: 60 },
-  { name: 'UI/UX Design', level: 'Intermediate', value: 65 },
+  {
+    name: 'HTML/CSS', level: 'Expert', value: 90, icon: '/icons/vue.svg'
+  },
+  {
+    name: 'UI/UX Design', level: 'Intermediate', value: 65, icon: '/icons/vue.svg'
+  },
 ]);
 
 const projects = ref([
