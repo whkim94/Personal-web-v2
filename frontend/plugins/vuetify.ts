66551f09a7b1cc85353cfec51c58209459a219f4
Steve, Kim <18128800+whkim94@users.noreply.github.com>
2024-01-27 01:36:20 -0800

    Fix: unnecessary option for vuetify

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -4,26 +4,9 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 import { createVuetify } from 'vuetify';
-import * as component from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-import * as labs from 'vuetify/labs/components'
-import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const vuetify = createVuetify({
-    components: {
-      ...component,
-      ...labs
-    },
-    directives,
-    icons: {
-      defaultSet: 'mdi',
-      aliases,
-      sets: {
-        mdi
-      }
-    }
-  });
+  const vuetify = createVuetify();
 
   nuxtApp.vueApp.use(vuetify);
 });
