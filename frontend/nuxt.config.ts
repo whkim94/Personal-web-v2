dbcf8e2881ce29c61a64d3f406d9d7925ae493e7
Jonathan Kim <whkim94@gmail.com>
2025-11-07 13:48:16 -0800

    Update: new web snippet

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -49,7 +49,7 @@ export default defineNuxtConfig({
           src: 'https://cdn.rankauthority.com/v2/seoscript.js',
           type: 'text/javascript',
           id: 'seo_automator',
-          'data-website-id': '3eb53654-1e45-4930-b2e7-3f6734929836'
+          'data-website-id': '63e77d50-60ed-4b27-87c9-f77b912c7d84'
         }
       ]
     },
